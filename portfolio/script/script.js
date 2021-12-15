7049a83431d866d96e4824b6f090c830d18169af
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-12-14 21:12:18 -0500

    massive update

---
+++ portfolio/script/script.js
@@ -0,0 +1,58 @@
+var modal = document.getElementById("myModal");
+var link = document.getElementsByClassName("mod");
+var sp = document.getElementsByClassName("end");
+var j;
+var k
+
+for (j = 0; j < link.length; j++) {
+  link[j].addEventListener("click", function() {
+    modal.style.display = "block";
+  });
+}
+
+for (k = 0; k < sp.length; k++) {
+  sp[k].addEventListener("click", function() {
+    modal.style.display = "none";
+  });
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    if (this.className === "open") {
+      this.className = "closed";
+    } else {
+      this.className = "open";
+    }
+
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+function slider() {
+  var checkBox = document.getElementById("check");
+  var dev = document.getElementById("dev");
+  var des = document.getElementById("des");
+  console.log(des);
+  if (checkBox.checked == true){
+    dev.classList.remove("hide");
+    des.classList.add("hide");
+  } else if (checkBox.checked == false){
+     des.classList.remove("hide");
+     dev.classList.add("hide");
+  }
+}
